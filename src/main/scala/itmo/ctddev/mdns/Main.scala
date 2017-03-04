22b2e9c9498be386177a18a72a6d310c99f2b7bd
Daniyar Itegulov <ditegulov@gmail.com>
2017-03-04 23:50:24 +0300

    Important fix

--- src/main/scala/itmo/ctddev/mdns/Main.scala
+++ src/main/scala/itmo/ctddev/mdns/Main.scala
@@ -11,5 +11,5 @@ import itmo.ctddev.mdns.core.MDNSNode
 object Main extends App {
   implicit val system = ActorSystem()
 
-  system.actorOf(Props(MDNSNode("sugak", new InetSocketAddress(args(0), args(1).toInt))))
+  system.actorOf(Props(MDNSNode("sugok", new InetSocketAddress(args(0), args(1).toInt))))
 }
